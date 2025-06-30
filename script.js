dcfc01221e9ab700034d9f8eaa6f3a617d768579
argeoff <214428272+Argeoff@users.noreply.github.com>
2025-06-30 17:23:16 +0800

    Excercise completed

--- script.js
+++ script.js
@@ -5,17 +5,26 @@ let humanScore = 0;
 let computerScore = 0;
 let humanChoice;
 let computerChoice;
+let gameRound = 1;
 
-playButton.addEventListener("click", ()=> {
+/*playButton.addEventListener("click", ()=> {
 	humanSelection = prompt ("Rock, Paper or Scissors?");
 	gethumanChoice(humanSelection);
 	getcomputerChoice();
 	playRound(humanChoice,computerChoice);
 	console.log("Scoreboard (Human vs Computer): " + humanScore + " , " + computerScore);
+});*/
+
+playButton.addEventListener("click", ()=> {
+	console.log("Game Round : " + gameRound);
+	gethumanChoice();
+	getcomputerChoice();
+	playGame();
 });
 
-function gethumanChoice(humanSelection){
-	humanChoice = humanSelection.toLowerCase();
+function gethumanChoice(){
+	humanChoice = prompt("Rock, Paper or Scissors?");
+	humanChoice = humanChoice.toLowerCase();
 	console.log("Human Choice: " + humanChoice);
 };
 
@@ -69,6 +78,25 @@ function playRound(humanChoice,computerChoice){
 			console.log ("You lose! " + computerChoice + " beats " + humanChoice);			
 		}
 	}	
+};
+
+function playGame() {
+	gameRound ++;
+	if (gameRound <= 5) {
+		playRound(humanChoice,computerChoice);
+	}
+	else {
+		if (humanScore === computerScore){
+			console.log ("Game Over! It is a TIE!");
+		}
+		else if (humanScore > computerScore){
+			console.log ("Game Over! You WIN!");			
+		}
+		else{
+			console.log ("Game Over! You LOSE!");
+		}
+	console.log (humanScore + " , " + computerScore);
+	}
 };
 
 
