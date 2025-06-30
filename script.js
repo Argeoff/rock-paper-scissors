ce91314dea4a277feef572d168ce204b4faa3e7b
argeoff <214428272+Argeoff@users.noreply.github.com>
2025-06-30 15:01:38 +0800

    javascript output human and computer choice via console.log

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 //Javascript for simple rock paper scrissors game
 
-function getHumanChoice(){
+const playButton = document.querySelector(".playButton");
+let humanScore = 0;
+let computerScore = 0;
+let humanChoice;
+let computerChoice;
+
+playButton.addEventListener("click", ()=> {
+	humanChoice = prompt ("Rock, Paper or Scissors?");
+	gethumanChoice(humanChoice);
+	getcomputerChoice();
+});
+
+function gethumanChoice(humanChoice){
+	humanChoice = humanChoice.toLowerCase();
+	console.log("Human Choice: " + humanChoice);
+};
+
+function getcomputerChoice (){
+	computerChoice = Math.floor(Math.random() * 3) +1;
+	switch (computerChoice){
+		case 1:
+		computerChoice = "rock";
+		break;
+		case 2:
+		computerChoice = "paper";
+		break;
+		case 3:
+		computerChoice = "scissors";
+		break;		
+	}
+	console.log("Computer Choice: " + computerChoice);
+};
+
+function playRound(humanChoice,computerChoice){
 };
